717b8de07281d5b8a7997746b7c5b12d5efe06ed
Aaron Thapa <notsmart98@gmail.com>
2024-08-10 23:33:18 +0545

    qemu guest

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,8 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  services.qemuGuest.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
