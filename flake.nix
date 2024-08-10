35ab6e449d4a99f09e2340f2f0f01ba09e79dd93
Aaron Thapa <notsmart98@gmail.com>
2024-08-10 23:26:56 +0545

    flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
