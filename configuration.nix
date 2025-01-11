7b3390f149b7f42924185195654de796f1e8d927
Aaron Thapa <notsmart98@gmail.com>
2025-01-11 21:13:10 +0545

    use vga too

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  boot.kernelParams = [ "console=ttyS0,115200n8" ];
+  boot.kernelParams = [ "console=ttyS0,115200n8" "console=tty0" ];
   boot.loader.grub.extraConfig = ''
     serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
     terminal_input serial
