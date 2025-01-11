4eb4fe6ed3af5e895fe79b61a7d3cf59add15a19
Aaron Thapa <notsmart98@gmail.com>
2025-01-11 21:01:22 +0545

    serial terminal

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,13 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
+  boot.kernelParams = [ "console=ttyS0,115200n8" ];
+  boot.loader.grub.extraConfig = ''
+    serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
+    terminal_input serial
+    terminal_output serial
+  '';
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   nix.settings.substituters = ["https://aseipp-nix-cache.freetls.fastly.net"];
